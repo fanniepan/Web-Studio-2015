ea73c847c88da2a2e6ea7973f28ffccab09e6447
fanniepan <f.pan95@gmail.com>
2015-11-20 23:26:35 -0500

    adding Final-Project

---
+++ Final-Project/js/main.js
@@ -0,0 +1,7 @@
+$("nav").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top - 76
+    });
+});
